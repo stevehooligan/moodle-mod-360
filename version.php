2c974b21a5361448c58649edf113120ba91b4c64
Andrew Lucas <andrew@pun.net>
2016-08-17 10:53:09 +0100

    marked as beta following testing by our customer rep

--- version.php
+++ version.php
@@ -26,9 +26,9 @@
 defined('MOODLE_INTERNAL') || die();
 
 /** @var stdClass $plugin */
-$plugin->version   = 2016081600;
+$plugin->version   = 2016081700;
 $plugin->requires  = 2013051400;; // See http://docs.moodle.org/dev/Moodle_Versions.
 $plugin->component = 'mod_threesixty';
-$plugin->maturity = MATURITY_ALPHA; // ...one of MATURITY_ALPHA, MATURITY_BETA, MATURITY_RC, MATURITY_STABLE.
+$plugin->maturity = MATURITY_BETA; // ...one of MATURITY_ALPHA, MATURITY_BETA, MATURITY_RC, MATURITY_STABLE.
 $plugin->cron      = 0;
-$plugin->release   = "1.1.0-alpha";
+$plugin->release   = "1.1.0-beta";
